314781c4e4405cded4e5109b6b06208f90a8259a
somename <someemail>
2015-10-15 12:11:35 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -11,19 +11,24 @@ public class Frame {
 	
 	//the score of a single frame
 	public int score(){
-		//to be implemented
-		return 0;
+		if (isStrike() == true) 
+			return firstThrow + bonus();
+		else if (isSpare() == true)
+			return firstThrow + secondThrow + bonus();
+			return firstThrow + secondThrow;
 	}
 
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
-		//to be implemented
+		if (firstThrow == 10)
+			return true;
 		return false;
 	}
 	
 	//return whether a frame is a spare or not
 	public boolean isSpare(){
-		//to be implemented
+		if (firstThrow + secondThrow == 10)
+			return true;
 		return false;
 	}
 
