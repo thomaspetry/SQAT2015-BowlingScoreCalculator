5bb62d2263ce0417f581f3ede79f4b2c5fbd8046
somename <someemail>
2015-10-15 13:09:26 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -16,9 +16,9 @@ public class BowlingGame {
 	}
 	
 	public void setBonus(int firstThrow, int secondThrow) {	
-		if (frames.get(frames.size()).isStrike())
+		if (frames.get(frames.size()-1).isStrike())
 			actualbonus = firstThrow + secondThrow;
-		if (frames.get(frames.size()).isSpare())
+		if (frames.get(frames.size()-1).isSpare())
 			actualbonus = firstThrow;
 	}
 	
